55bddd4dd3d22f244c09ef29e888b731e3a6c3d7
bruk19 <brukteshome21@gmail.com>
2022-09-21 15:31:55 +0300

    Add post table

--- db/migrate/20220921122917_create_user.rb
+++ db/migrate/20220921122917_create_user.rb
@@ -1,10 +1,10 @@
 class CreateUser < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :Title
-      t.text :Text
-      t.integer :CommentsCounter
-      t.integer :LikesCounter
+      t.string :Name
+      t.string :Photo
+      t.text :Bio
+      t.integer :PostCounter
 
       t.timestamps
     end
