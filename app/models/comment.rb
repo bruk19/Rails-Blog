97899004293ca25ee5a73c83d6ecbd1f1186fa57
bruk19 <brukteshome21@gmail.com>
2022-09-22 17:16:18 +0300

    Create a method that update comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,11 @@
 class  Comment < ApplicationRecord
     belongs_to :user, class_name: "User", foreign_key: "AuthorId"
     belongs_to :post, class_name: "Post", foreign_key: "PostId"
+
+    after_save :update_counter
+
+    def update_counter
+        post.CommentsCounter.nil? ? post.CommentsCounter = 1 : post.CommentsCounter += 1
+        post.update(CommentsCounter: post.CommentsCounter)
+    end
 end
