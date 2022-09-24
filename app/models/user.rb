b4db416989af1abfe0c9502ab90bb45f85c6796a
bruk19 <brukteshome21@gmail.com>
2022-09-24 09:16:31 +0300

    Add validation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  validates :Name, presence: true
+  validates :PostCounter, Numericality: {only_integer: true, greater_than_or_equal_to: 0}
+
   has_many :posts, class_name: 'Post', foreign_key: 'AuthorId'
   has_many :comments, class_name: 'Comment', foreign_key: 'AuthorId'
   has_many :likes, class_name: 'Like', foreign_key: 'AuthorId'
