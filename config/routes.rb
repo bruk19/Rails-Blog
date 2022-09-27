927452df3b1cf7fa0ec1231b02216fc528722b98
bruk19 <brukteshome21@gmail.com>
2022-09-27 12:30:18 +0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   root 'users#index'
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 end
