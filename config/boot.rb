e82d0b4dc62e668103a5b9edcf17c24fd074bc15
bruk19 <brukteshome21@gmail.com>
2022-09-27 12:33:04 +0300

    Add fix linters

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,4 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
-require 'bundler/setup' 
+require 'bundler/setup'
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   root 'users#index'
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show]
   end
 end
