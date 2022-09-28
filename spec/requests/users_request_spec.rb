a32da8e0dd773ed2daa526cad3b865af14574fb3
bruk19 <brukteshome21@gmail.com>
2022-09-28 09:50:23 +0300

    Add test for user index

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
+    describe 'GET /index' do
+        it 'Return http success' do
+          get '/users/index'
+          expect(response).to have_http_status(:success)
+        end
+    end
 end
