f57ceafd7a7ff3eaadb2d825f10ffe929e1bf951
bruk19 <brukteshome21@gmail.com>
2022-09-28 09:49:38 +0300

    Add test for post index

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
+    describe 'GET /index' do
+        it 'Return http success' do 
+          get '/posts/index'
+          expect(response).to have_http_status(:success)
+        end
+    end
 end
