cc91b140ef9740bd672b07c5381c2e74fbcd1cac
bruk19 <brukteshome21@gmail.com>
2022-09-28 10:04:29 +0300

    Add test for check if post show templates render correctly

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -14,4 +14,14 @@ RSpec.describe 'Posts', type: :request do
           expect(response).to have_http_status(:success)
         end
     end
+
+    it 'Should render the index template' do 
+        get '/posts/show'
+        expect(response).to render_template(:show)
+    end
+
+    it 'does not render a different template' do 
+        get '/posts/show'
+        expect(response).to_not render_template(:index)
+    end
 end
