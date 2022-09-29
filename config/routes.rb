410932cfb4decc9b79ce0f568fb01121dd5df3b3
bruk19 <brukteshome21@gmail.com>
2022-09-29 13:53:22 +0300

    Implement route with partials

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  root 'users#index'
   get 'users/index'
   get 'users/show'
   get 'posts/index'
