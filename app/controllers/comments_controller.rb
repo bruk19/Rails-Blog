c7f9052a73db166de969a07dc3d080a8901369b1
bruk19 <brukteshome21@gmail.com>
2022-10-01 09:23:24 +0300

    Create comment controllers

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,20 @@
+class CommentsController < ApplicationController
+    def create
+      @post = Post.find(params[:post_id])
+      @comment = Comment.create(comment_params)
+      @comment.user = current_user
+      @comment.post = @post
+      if @comment.save
+        flash[:success] = 'New Comment created successfully'
+        redirect_to user_post_path(current_user, @post)
+      else
+        flash.now[:error] = 'An error occurred : Comment could not be created'
+      end
+    end
+
+    private
+
+    def comment_params
+      params.require(:comment).permit(:Text)
+    end
+  end
