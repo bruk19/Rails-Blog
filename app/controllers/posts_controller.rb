36aacae555eacec21efaee3fce026108222339ae
bruk19 <brukteshome21@gmail.com>
2022-10-01 10:06:07 +0300

    Add required changes according to the reviewer

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,10 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def new
+    @post = Post.new
+  end
+
   def create
     @post = Post.create(post_params)
     @post.user = current_user
