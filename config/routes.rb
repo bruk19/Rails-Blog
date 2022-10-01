20a00d924d1212df9f9fa55ef778c69268cb5dd7
bruk19 <brukteshome21@gmail.com>
2022-10-01 09:38:59 +0300

    Add form in the routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,12 @@ Rails.application.routes.draw do
   get 'posts/index'
   get 'posts/show'
 
-  resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :new, :form, :create]
+  end
+
+  resources :posts do
+    resources :comments, only: [:create]
+    resources :likes, only: [:create]
   end
 end
