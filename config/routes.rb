46855713831edf7d90a0b0a0e95bc71d7a3ba0a2
bruk19 <brukteshome21@gmail.com>
2022-10-06 13:13:24 +0300

    update routes and development

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  devise_for :users
+  devise_scope  :user do
+    get 'users/sign_out' => 'devise/sessions#destroy'
+  end
   root 'users#index'
 
   resources :users, only: [:index, :show] do
