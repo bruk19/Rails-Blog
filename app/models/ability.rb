9a24a43ff2a43cf9db6313f8e331a08b8bc1a714
bruk19 <brukteshome21@gmail.com>
2022-10-13 13:32:50 +0300

    Generate ablity file and add rules

---
+++ app/models/ability.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    if user.nil?
+      can :read, Post
+    elsif user.role == 'admin'
+      can :manage, Post
+    else
+      can [:read, :create], Post
+      can [:update :destroy], Post, { Author_id: user.id }
+    end
+  end
+end
