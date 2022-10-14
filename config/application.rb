3483bcb91bd727cd6e42ce165c0e99a3f7862459
bruk19 <brukteshome21@gmail.com>
2022-10-14 09:53:10 +0300

    Add routes

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+config.api_only = true
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
--- config/routes.rb
+++ config/routes.rb
@@ -25,9 +25,9 @@ Rails.application.routes.draw do
 
   namespace :api do 
     namespace :v1 do
-      resources :users, only: [index] do
-        resources :posts, only: [index, show] do
-          resources :comments, only: [new, create] do
+      resources :users, only: [:index] do
+        resources :posts, only: [:index, :show] do
+          resources :comments, only: [:new, :create] do
           end
         end
       end
